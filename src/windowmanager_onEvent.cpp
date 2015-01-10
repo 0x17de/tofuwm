d719ae741bd08961da13d246ff52ff603a7993c3
Manuel Herrmann <it@icetruck.de>
2015-01-10 17:27:28 +0100

    cleanup

--- src/windowmanager_onEvent.cpp
+++ src/windowmanager_onEvent.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <sstream>
-#include <cmath>
 #include <X11/Xlib.h>
 #include "windowmanager.h"
 
@@ -14,58 +13,6 @@ void WindowManager::onMapRequest() {
     } else {
         XMapWindow(display, event.xmaprequest.window);
     }
-}
-
-void WindowManager::onKeyPress() {
-    if (event.xkey.state & keyGrabber->defaultModifier()) {
-        if (event.xkey.state & ShiftMask) {
-            if (event.xkey.keycode == keyGrabber->keyClose()) {
-                addDebugText("WINDOW CLOSE");
-                if (currentWindow)
-                    currentWindow->close();
-                currentWindow = 0; // @TODO: Select next window by mouse position
-            }
-        } else {
-            if (event.xkey.keycode == keyGrabber->keyWorkspace1()) {
-                addDebugText("WORKSPACE 1");
-                changeWorkspace(0);
-            } else if (event.xkey.keycode == keyGrabber->keyWorkspace2()) {
-                addDebugText("WORKSPACE 2");
-                changeWorkspace(1);
-            } else if (event.xkey.keycode == keyGrabber->keyDMenu()) {
-                addDebugText("DMENU SPAWN");
-                char *const parmList[] = {(char *) "dmenu_run", 0};
-                spawn("/usr/bin/dmenu_run", parmList);
-            }
-        }
-    }
-}
-
-void WindowManager::onKeyRelease() {
-}
-
-void WindowManager::onButtonPress() {
-    if (event.xbutton.state & keyGrabber->defaultModifier()) {
-        if (event.xbutton.subwindow) {
-            moveWindow = findWindow(event.xbutton.subwindow);
-            if (!moveWindow)
-                return;
-            currentWindow = moveWindow;
-            currentWindow->setActive(true);
-            XGetWindowAttributes(display, moveWindow->frame, &moveWindowAttributes);
-            moveWindowStart = event.xbutton;
-            XRaiseWindow(display, moveWindow->frame);
-            if (moveWindowStart.button == 3) { // Right mouse
-                moveWindowExpandXPositive = moveWindowStart.x_root >= moveWindowAttributes.x + moveWindowAttributes.width / 2;
-                moveWindowExpandYPositive = moveWindowStart.y_root >= moveWindowAttributes.y + moveWindowAttributes.height / 2;
-            }
-        }
-    }
-}
-
-void WindowManager::onButtonRelease() {
-    moveWindowStart.subwindow = None;
-    moveWindow = 0;
 }
 
 void WindowManager::onEnter() {
@@ -82,21 +29,6 @@ void WindowManager::onLeave() {
 
     currentWindow->setActive(false);
     setCurrentWindow(0);
-}
-
-void WindowManager::onMotion() {
-    if (event.xbutton.state & keyGrabber->defaultModifier()) {
-        if (moveWindow) {
-            int xdiff = event.xbutton.x_root - moveWindowStart.x_root;
-            int ydiff = event.xbutton.y_root - moveWindowStart.y_root;
-            if (moveWindowStart.button == 1) { // Left mouse
-                moveWindow->relocate(moveWindowAttributes.x + xdiff, moveWindowAttributes.y + ydiff, moveWindowAttributes.width, moveWindowAttributes.height);
-            } else if (moveWindowStart.button == 3) { // Right mouse
-                moveWindow->relocate(moveWindowAttributes.x + (moveWindowExpandXPositive ? 0 : xdiff), moveWindowAttributes.y + (moveWindowExpandYPositive ? 0 : ydiff),
-                    moveWindowAttributes.width + (moveWindowExpandXPositive ? xdiff : -xdiff), moveWindowAttributes.height + (moveWindowExpandYPositive ? ydiff : -ydiff));
-            }
-        }
-    }
 }
 
 void WindowManager::onConfigureRequest() { // @TODO: fix window jumping
